f71e9d240644f550b2773645b9e0074c9c92e498
Thiago Aragão <thiago.alpha.06@gmail.com>
2024-07-08 19:17:11 -0300

    Correção gradient boost

--- gradientBoost.py
+++ gradientBoost.py
@@ -1,183 +1,31 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "5f7340cc",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import pandas as pd\n",
-    "import numpy as np\n",
-    "import matplotlib.pyplot as plt\n",
-    "import seaborn as sns\n",
-    "%matplotlib inline\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "91072846",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "obesity = pd.read_csv('final.csv')"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "91e1d174",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "obesity.head()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "a6c4f886",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "from sklearn.model_selection import train_test_split\n",
-    "from sklearn.model_selection import GridSearchCV\n",
-    "from sklearn.ensemble import GradientBoostingClassifier\n",
-    "\n",
-    "parameters = [{'learning_rate': [0.1,0.3,0.5], 'n_estimators': [100,200], 'max_depth': [2,3,4,5], 'min_impurity_decrease': [0.0,0.1,0.3,0.5]}]\n",
-    "\n",
-    "X = obesity.drop('NObeyesdad', axis = 1)\n",
-    "y = obesity['NObeyesdad']\n",
-    "\n",
-    "X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)\n",
-    "\n",
-    "grad=GradientBoostingClassifier()\n",
-    "\n",
-    "GS = GridSearchCV(estimator = grad, \n",
-    "                   param_grid = parameters, \n",
-    "                   scoring = 'accuracy', \n",
-    "                   refit = 'accuracy',\n",
-    "                   cv = 5,\n",
-    "                   verbose = 4 ,\n",
-    "                  error_score='raise'\n",
-    "                 )\n",
-    "\n",
-    "GS.fit(X_train, y_train)\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "0f26c7df",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "GS.best_params_"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "8585e505",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "252ab37f",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "fab3e2d5",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "a1ec53bb",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "0e4e36f3",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "96b9e174",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "b1a58837",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "61eb330c",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "9bf54cdb",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "fda0762c",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+obesity = pd.readcsv('final.csv')
+obesity.head()
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import GradientBoostingClassifier
+
+parameters = [{'learning_rate': [0.1,0.3,0.5], 'n_estimators': [100,200], 'max_depth': [2,3,4,5], 'min_impurity_decrease': [0.0,0.1,0.3,0.5]}]
+
+X = obesity.drop('NObeyesdad', axis = 1)
+y = obesity['NObeyesdad']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)
+
+grad=GradientBoostingClassifier()
+
+GS = GridSearchCV(estimator = grad, 
+                   param_grid = parameters, 
+                   scoring = 'accuracy', 
+                   refit = 'accuracy',
+                   cv = 5,
+                   verbose = 4 ,
+                  error_score='raise'
+                 )
+
+GS.fit(X_train, y_train)
+GS.best_params
