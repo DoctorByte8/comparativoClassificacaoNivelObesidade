41526ec6d6817ef40808f91b83187fdece2de457
felipemalli <felipevahia@gmail.com>
2024-07-08 20:39:42 -0300

    ajust random forest

--- random-forest.py
+++ random-forest.py
@@ -1,15 +1,15 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.preprocessing import LabelEncoder
-import matplotlib.pyplot as plt  # Adicione esta importação no início do arquivo
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Carrega os dados
-file_path = 'ObesityDataSet_raw_and_data_sinthetic_nonDuplicates.csv'
+file_path = 'after_preprocessing.csv'
 data = pd.read_csv(file_path)
 
-# Necessário transformar as strings em forma numérica. Encoders são usados para isso.
 label_encoders = {}
 for column in data.columns:
     if data[column].dtype == type(object):
@@ -17,76 +17,70 @@ for column in data.columns:
         data[column] = le.fit_transform(data[column])
         label_encoders[column] = le
 
-# Separar os dados em variáveis de entrada (X) e de saída (Y) esperada.
 X = data.drop('NObeyesdad', axis=1)
 y = data['NObeyesdad']
 
-# Divide os dados em 80% para treinamento (X_train e y_train) e 20% para teste (X_test e y_test).
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Cria o modelo Random Forest com os atributos abaixo.
 model = RandomForestClassifier(
-    criterion='gini',  # Utiliza o critério de impureza gini.
-    min_weight_fraction_leaf=0.0, # Fração ponderada mínima da soma total de pesos (de todas as amostras de entrada) necessária para estar em um nó folha.
-    max_features='sqrt', # Número de recursos a serem considerados ao procurar a melhor divisão em cada nó de árvore, sqrt equivale a usar a raiz quadrada do número total de recursos.
-    max_leaf_nodes=None, # Número máximo de nó folhas.
-    min_impurity_decrease=0.0, # Um nó será dividido se essa divisão induzir uma diminuição da impureza maior ou igual a esse valor. 0 significa que qualquer divisão será considerada, permitindo crescimento à vontade.
-    bootstrap=True, # ao construir cada árvore de decisão, pode selecionar aleatoriamente observações do conjunto de dados original e assim permitir que a mesma observação seja selecionada mais de uma vez, mais eficaz.
-    oob_score=True, # se deve usar amostras out-of-bag para estimar a precisão
-    n_jobs=None, # None significa que vai usar todos os processadores
-    random_state=42, # Um estado aleatório foi definido como 42 para garantir que o modelo retornará o mesmo resultado sempre, para conseguirmos depurar e comparar modelos.
-    verbose=0, # Controla a verbosidade ao ajustar e prever.
-    warm_start=False, # Não reutiliza nenhuma solução da chamada anterior.
-    class_weight=None, # Não considera o peso das classes para ajustar o modelo.
-    ccp_alpha=0.0, # Sem um um parâmetro de complexidade para a poda de custo-complexidade mínima.
-    max_samples=None # Número máximo de amostras a serem usadas para treinar cada árvore individual.
+    criterion='gini',
+    min_weight_fraction_leaf=0.0,
+    max_features='sqrt',
+    max_leaf_nodes=None,
+    min_impurity_decrease=0.0,
+    bootstrap=True,
+    oob_score=True,
+    n_jobs=None,
+    random_state=42,
+    verbose=0,
+    warm_start=False,
+    class_weight=None,
+    ccp_alpha=0.0,
+    max_samples=None
 )
 model.fit(X_train, y_train)
 
-# Define o dicionário de parâmetros para o GridSearchCV
 param_grid = {
-    'n_estimators': [100, 200, 300], # Treina o modelo com n árvores de decisão.
-    'max_depth': [None, 10, 20, 30], # Define a profundidade máxima das árvores.
-    'min_samples_split': [2, 5, 10], # Número mínimo de amostras necessárias para dividir um nó interno.
-    'min_samples_leaf': [1, 2, 4], # Número mínimo de amostras necessárias para estar em um nó folha.
+    'n_estimators': [100, 200, 300],
+    'max_depth': [None, 10, 20, 30],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4]
 }
 
-# Configura o GridSearchCV
 grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, verbose=2, n_jobs=-1, error_score='raise')
 
-# Treina o GridSearchCV
-try:
-    grid_search.fit(X_train, y_train)
-except ValueError as e:
-    print("Erro durante o ajuste do modelo:", e)
+grid_search.fit(X_train, y_train)
 
-# Imprime os melhores parâmetros e a melhor pontuação
-print("Melhores parâmetros:", grid_search.best_params_)
-print("Melhor pontuação:", grid_search.best_score_)
-
-# Imprime a importância de cada recurso
 feature_importances = pd.Series(grid_search.best_estimator_.feature_importances_, index=X.columns)
 print("Importância de cada recurso:")
 print(feature_importances.sort_values(ascending=False))
 
+plt.figure(figsize=(12, 8))
+sns.barplot(x=feature_importances.sort_values(ascending=False), y=feature_importances.sort_values(ascending=False).index)
+plt.title('Importância de cada recurso no modelo Random Forest')
+plt.xlabel('Grau de importância')
+plt.ylabel('Recursos')
+plt.savefig('importancia_recursos.png')
+plt.close()
 
-# # Gerar PNG com a importância dos recursos
-# # Plotando a importância dos recursos
-# plt.figure(figsize=(12, 8))
-# feature_importances.sort_values().plot(kind='barh')
-# plt.title('Importância de cada recurso no modelo Random Forest')
-# plt.xlabel('Grau de importância')
-# plt.ylabel('Recursos')
-# plt.savefig('/home/x/feature_importances.png')  # defina o caminho do arquivo PNG
-# plt.close()  # Fecha a figura para liberar memória
+print("Melhores parâmetros:", grid_search.best_params_)
+print(f"Acurácia de teste: {grid_search.best_score_:.3f}")
 
-# Verifica se o melhor modelo usa bootstrap
-if grid_search.best_estimator_.get_params()['bootstrap']:
-    print("Score OOB:", grid_search.best_estimator_.oob_score_)
-else:
-    print("OOB score não disponível, pois bootstrap está desativado.")
+y_pred_val = grid_search.best_estimator_.predict(X_validation)
 
-# Faz a predição com base nos dados de teste e avalia a acurácia do modelo.
-y_pred = grid_search.best_estimator_.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Acurácia: {accuracy:.2f}')
+class_names = label_encoders['NObeyesdad'].classes_
+
+conf_matrix_val = confusion_matrix(y_validation, y_pred_val)
+
+accuracy_val = accuracy_score(y_validation, y_pred_val)
+print(f"Acurácia de validação: {accuracy_val:.3f}")
+
+plt.figure(figsize=(16, 8))
+ax = sns.heatmap(conf_matrix_val, annot=True, fmt="d", cmap="Blues", xticklabels=class_names, yticklabels=class_names, annot_kws={"size": 10})
+ax.set_xticklabels(ax.get_xticklabels(), rotation=0, fontsize=10)
+ax.set_yticklabels(ax.get_yticklabels(), fontsize=10)
+plt.xlabel('Predito', fontsize=12)
+plt.ylabel('Real', fontsize=12)
+plt.title('Matriz de Confusão - Conjunto de Validação', fontsize=14)
+plt.savefig('matriz_confusao_validacao.png')
+plt.close()
